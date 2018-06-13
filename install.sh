4ac9300d623f15af6933fa1b4e18a729f1a67e01
Robin Peters <robin.peters@ufz.de>
2018-06-13 15:11:37 +0200

    fixed install.sh

--- install.sh
+++ install.sh
@@ -4,15 +4,14 @@
 
 cp -r busylight /usr/local/busylight
 BASE=/usr/local/busylight
-chown -R $USER $BASE
-cd $BASE
+chown -R $SUDO_USER:$(id -gn $SUDO_USER) $BASE
 
 ###
 
 git clone https://github.com/porsager/busylight.git $BASE/API
 cd $BASE/API
-npm install .
-chown -R $USER $BASE
+chown -R $SUDO_USER:$(id -gn $SUDO_USER) $BASE
+su $SUDO_USER -c 'npm install .'
 
 ###
 
